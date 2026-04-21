1917f399c32d11d856f9e52a1e8bf6803580d7f5
sos-cyber <shinohshim@gmail.com>
2026-04-21 10:40:03 +0900

    feat : 이름, 나이, 전화번호 정규식 처리 및 예외처리 완료

--- src/TelBookMain.java
+++ src/TelBookMain.java
@@ -1,10 +1,11 @@
+import exception.MyException;
 import view.UserView;
 
 import java.util.Locale;
 import java.util.Scanner;
 
 public class TelBookMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException {
         Scanner sc = new Scanner(System.in);
         //  UserView 인스턴스를 생성
         UserView userView = new UserView(sc);
--- src/view/UserView.java
+++ src/view/UserView.java
@@ -1,5 +1,8 @@
 package view;
 
+import exception.InputValidation;
+import exception.MyException;
+
 import java.util.Scanner;
 
 public class UserView {
@@ -10,7 +13,34 @@ public class UserView {
     }
 
 
-    public void insert() {
+    public void insert() throws MyException{
+        //  검증 클래스 생성
+        InputValidation validation = new InputValidation();
+        System.out.println("== 전화번호 등록 ==");
+        //  이름이 올바른 값이 들어올 때 까지 반복
+        boolean nameOk = false;
+        do{
+            try {
+            //  이름 : 무조건 한글만, 중간공백 없이
+            System.out.println("이름 : ");
+            String name = scanner.next();
+            validation.nameCheck(name);
+            nameOk = true;
+        }
+        catch (MyException e){
+            System.out.println(e.getMessage());
+        }
+        }while (!nameOk);
+
+        //  나이 : 0세 ~ 120세 사이값
+        System.out.println("나이 : ");
+        int age = scanner.nextInt();
+        //  전화번호 : 010-XXXX-XXXX
+        System.out.println("전화번호 : ");
+        String phone = scanner.next();
+        System.out.println("주소 : ");
+        String address = scanner.next();
+
     }
 
     public void update() {
